20334d8aab35d7be380d1d6786b3542ea1957c7b
Miguel Ramos <145162809+rMigueLima@users.noreply.github.com>
2023-11-04 15:45:52 -0300

    Update footerPerfilTinder.js

--- Componentes/footerPerfilTinder.js
+++ Componentes/footerPerfilTinder.js
@@ -1,29 +1,40 @@
-import {View , Image} from 'react-native';
+import {View , Image, TouchableOpacity} from 'react-native';
 import perfil from '../estilizar/perfilEstiliza.js';
+import { Button } from 'react-native-web';
 
 export default function nav() {
     return(
         <View style={perfil.footer}>
+            <TouchableOpacity>
             <Image
-                source={require('../assets/tinderLogoFooter.png')}
+                source={require('../assets/fogoFooter.png')}
                 style={perfil.tinderFooter}
             />
-            <Image  
-                source={require('../assets/estrelaFooter.png')}
-                style={perfil.estrelaFooter}
-            />
+            </TouchableOpacity>
+            <TouchableOpacity>
             <Image  
                 source={require('../assets/searchFooter.png')}
                 style={perfil.searchFooter}
             />
+            </TouchableOpacity>
+            <TouchableOpacity>
             <Image  
-                source={require('../assets/comentFooter.png')}
+                source={require('../assets/starFooter.jpg')}
+                style={perfil.estrelaFooter}
+            />
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <Image  
+                source={require('../assets/commentFooter.png')}
                 style={perfil.commentFooter}
             />
+            </TouchableOpacity>
+            <TouchableOpacity>
             <Image  
                 source={require('../assets/personFooter.png')}
                 style={perfil.personFooter}
             />
+            </TouchableOpacity>
 
         </View>
     )
